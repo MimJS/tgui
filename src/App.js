8f207774998fe213d4e84722e966110a6766c2d7
mimjs <mikhailmateevskiy@gmail.com>
2023-01-31 00:29:40 +0300

    height set

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 function App() {
   return (
     <div className="App">
-      <div style={{ background: "var(--tg-theme-bg-color)" }}>bg_color</div>
-      <div style={{ background: "var(--tg-theme-text-color)" }}>text_color</div>
-      <div style={{ background: "var(--tg-theme-hint-color)" }}>hint_color</div>
-      <div style={{ background: "var(--tg-theme-link-color)" }}>link_color</div>
-      <div style={{ background: "var(--tg-theme-button-color)" }}>
+      <div style={{ background: "var(--tg-theme-bg-color)", height: 50 }}>
+        bg_color
+      </div>
+      <div style={{ background: "var(--tg-theme-text-color)", height: 50 }}>
+        text_color
+      </div>
+      <div style={{ background: "var(--tg-theme-hint-color)", height: 50 }}>
+        hint_color
+      </div>
+      <div style={{ background: "var(--tg-theme-link-color)", height: 50 }}>
+        link_color
+      </div>
+      <div style={{ background: "var(--tg-theme-button-color)", height: 50 }}>
         button_color
       </div>
-      <div style={{ background: "var(--tg-theme-button-text-color)" }}>
+      <div
+        style={{ background: "var(--tg-theme-button-text-color)", height: 50 }}
+      >
         button_text_color
       </div>
-      <div style={{ background: "var(--tg-theme-secondary-bg-color)" }}>
+      <div
+        style={{ background: "var(--tg-theme-secondary-bg-color)", height: 50 }}
+      >
         secondary_bg_color
       </div>
     </div>
